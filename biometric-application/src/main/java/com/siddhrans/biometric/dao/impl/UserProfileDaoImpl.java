36f6601220ee4479dcca0d1c9d76cf947acb2361
AIM Technology <anithmarnoor007@gmail.com>
2017-06-09 18:33:09 +0530

    Added Missing files in last commit

---
+++ biometric-application/src/main/java/com/siddhrans/biometric/dao/impl/UserProfileDaoImpl.java
@@ -0,0 +1,38 @@
+package com.siddhrans.biometric.dao.impl;
+
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import com.siddhrans.biometric.dao.AbstractDao;
+import com.siddhrans.biometric.dao.UserProfileDao;
+import com.siddhrans.biometric.model.UserProfile;
+ 
+
+ 
+ 
+@Repository("userProfileDao")
+public class UserProfileDaoImpl extends AbstractDao<Integer, UserProfile>implements UserProfileDao{
+ 
+    public UserProfile findById(int id) {
+        return getByKey(id);
+    }
+ 
+    public UserProfile findByType(String type) {
+        Criteria crit = createEntityCriteria();
+        crit.add(Restrictions.eq("type", type));
+        return (UserProfile) crit.uniqueResult();
+    }
+     
+    @SuppressWarnings("unchecked")
+    public List<UserProfile> findAll(){
+        Criteria crit = createEntityCriteria();
+        crit.addOrder(Order.asc("type"));
+        return (List<UserProfile>)crit.list();
+    }
+     
+}
