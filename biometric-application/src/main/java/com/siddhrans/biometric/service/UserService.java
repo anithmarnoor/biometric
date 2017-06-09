2d94eed8c1db1636a631f9b43aec7d660d120c01
AIM Technology <anithmarnoor007@gmail.com>
2017-06-10 04:01:09 +0530

    Login Application

---
+++ biometric-application/src/main/java/com/siddhrans/biometric/service/UserService.java
@@ -0,0 +1,26 @@
+package com.siddhrans.biometric.service;
+
+import java.util.List;
+
+import com.siddhrans.biometric.model.User;
+
+
+ 
+ 
+public interface UserService {
+     
+    User findById(int id);
+     
+    User findBySSO(String sso);
+     
+    void saveUser(User user);
+     
+    void updateUser(User user);
+     
+    void deleteUserBySSO(String sso);
+ 
+    List<User> findAllUsers(); 
+     
+    boolean isUserSSOUnique(Integer id, String sso);
+ 
+}
---
+++ biometric-application/src/main/resources/initialData.sql
@@ -0,0 +1,19 @@
+INSERT INTO USER_PROFILE(type)
+VALUES ('USER');
+  
+INSERT INTO USER_PROFILE(type)
+VALUES ('ADMIN');
+  
+INSERT INTO USER_PROFILE(type)
+VALUES ('DBA');
+  
+  
+/* Populate one Admin User which will further create other users for the application using GUI Username is:sam, Password is abc125 */
+INSERT INTO APP_USER(sso_id, password, first_name, last_name, email)
+VALUES ('sam','$2a$10$4eqIF5s/ewJwHK1p8lqlFOEm2QIA0S8g6./Lok.pQxqcxaBZYChRm', 'Sam','Smith','samy@xyz.com');
+  
+  
+/* Populate JOIN Table */
+INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
+  SELECT user.id, profile.id FROM app_user user, user_profile profile
+  where user.sso_id='sam' and profile.type='ADMIN';
